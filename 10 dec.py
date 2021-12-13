d09d8c4784487ccf30240325a1231e6979192720
vanshpb <96041495+vanshpb@users.noreply.github.com>
2021-12-13 12:25:33 +0530

    Add files via upload

---
+++ 10 dec.py
@@ -0,0 +1,80 @@
+"***welcome to quiz game***"
+print("do you want to play quiz")
+print("1. yes")
+print("2. no")
+ch=int(input("choose :"))
+if ch==1 :
+    print("ok,here is your first question")
+    print("what is the capital of the INDIA?")
+    print("1. mumbai")
+    print("2. delhi")
+    print("3. kolkata")
+mn=int(input("answer:"))
+if mn==2:
+    print("yeah,you got the 20 points")
+else:
+    print("wrong,try again")
+print("are you ready for the second round")
+print("1. yes")
+print("2. no")
+ch=int(input("choose :"))
+if ch==1 :
+    print("ok,here is your second question")
+    print("what is the capital of the PAKISTAN?")
+    print("1. mumbai")
+    print("2. delhi")
+    print("3. islamabaad")
+mn=int(input("answer:"))
+if mn==3:
+    print("yeah,you got the 20 points more")
+    print("total point==40")
+else:
+    print("wrong,try again")
+print("are you ready for the third round")
+print("1. yes")
+print("2. no")
+ch=int(input("choose :"))
+if ch==1 :
+    print("ok,here is your third question")
+    print("what is the capital of the france?")
+    print("1. paris")
+    print("2. bangkok")
+    print("3. islamabaad")
+mn=int(input("answer:"))
+if mn==1:
+    print("yeah,you got the 20 points more")
+    print("total point==60")
+else:
+    print("wrong,try again")
+print("are you ready for the fourth round")
+print("1. yes")
+print("2. no")
+ch=int(input("choose :"))
+if ch==1 :
+    print("ok,here is your fourth question")
+    print("what is the capital of the turkey?")
+    print("1. london")
+    print("2. delhi")
+    print("3. ankara")
+mn=int(input("answer:"))
+if mn==3:
+    print("yeah,you got the 20 points more")
+    print("total point==80")
+else:
+    print("wrong,try again")    
+print("are you ready for the fifth round")
+print("1. yes")
+print("2. no")
+ch=int(input("choose :"))
+if ch==1 :
+    print("ok,here is your fifth question")
+    print("what is the capital of the U.S.A?")
+    print("1. abu dhabi")
+    print("2. delhi")
+    print("3. washington Dc")
+mn=int(input("answer:"))
+if mn==3:
+    print("yeah,you got the 20 points more")
+    print("total point==100")
+else:
+    print("wrong,try again")
